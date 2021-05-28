5c8979bc1a0fbb4e37270cc09edde42b1cccdd70
onebabyq <66979358+onebabyq@users.noreply.github.com>
2021-05-28 14:47:37 +0700

    load AIP, inssert

--- Lab_ThuongKi/app/src/main/java/com/example/baitapthuongki/MainActivity.java
+++ Lab_ThuongKi/app/src/main/java/com/example/baitapthuongki/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.baitapthuongki;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -14,18 +19,24 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<User> listUser;
     UserAdapter adapter;
     ListView listView;
-
+    Button btn_add;
+    EditText etName,etAge;
+    Gson gson = new Gson();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,17 +50,87 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void anhXa() {
+        etName = (EditText) findViewById(R.id.etName);
+        etAge = (EditText) findViewById(R.id.etAge) ;
+        btn_add = (Button) findViewById(R.id.btn_add) ;
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PostApi(etName.getText().toString(),etAge.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
         listView = (ListView) findViewById(R.id.listView);
         listUser = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
-            getJson("https://60ad91b980a61f00173312de.mockapi.io/users/" + i);
-        }
+        getUsers();
+//        for (int i = 1; i <= 14; i++) {
+//            getJson("https://60ad91b980a61f00173312de.mockapi.io/users/" + i);
+//        }
 //        listUser.add(new User(1,"Hoang Son",18));
 //        listUser.add(new User(2,"Hoang Son",18));
 //        listUser.add(new User(3,"Hoang Son",18));
 
     }
+    public void getUsers() {
+        listUser = new ArrayList<>();
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://60ad91b980a61f00173312de.mockapi.io/users";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject object = (JSONObject) response.get(i);
+                        listUser.add(gson.fromJson(String.valueOf(object), User.class));
+                        adapter.notifyDataSetChanged();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, error -> Toast.makeText(MainActivity.this, "Error with JSON Array Object", Toast.LENGTH_SHORT).show());
 
+        queue.add(jsonArrayRequest);
+    }
+    private void PostApi(String name, String age){
+        String url = "https://60ad91b980a61f00173312de.mockapi.io/users";
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this, "Successfully", Toast.LENGTH_SHORT).show();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error by Post data!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                HashMap<String, String>
+                        params = new HashMap<>();
+                params.put("name", name);
+                params.put("age", age);
+                User user = new User();
+                user.setName(name);
+                user.setAge(Integer.parseInt(age));
+                listUser.add(user);
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
     public void getJson(String url) {
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
