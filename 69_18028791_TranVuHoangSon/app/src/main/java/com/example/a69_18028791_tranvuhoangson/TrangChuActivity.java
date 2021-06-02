5cb32943121c5c5e5754df1813a75b1b438569d3
onebabyq <66979358+onebabyq@users.noreply.github.com>
2021-06-02 07:57:21 +0700

    commit man hinh login + hinhanh

---
+++ 69_18028791_TranVuHoangSon/app/src/main/java/com/example/a69_18028791_tranvuhoangson/TrangChuActivity.java
@@ -0,0 +1,14 @@
+package com.example.a69_18028791_tranvuhoangson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrangChuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang_chu);
+    }
+}
