3de68cc6a247408da2ea2bbb8f292753aa7e1502
onebabyq <66979358+onebabyq@users.noreply.github.com>
2021-06-20 12:46:15 +0700

    commit khoi tao project

---
+++ app/src/main/java/com/example/a64_18028791_tranvuhoangson/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a64_18028791_tranvuhoangson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
